fddabff9de244edb0034602c3c30d09c2488a27a
Mohammad Ghorbani <ghorbani-m@aut.ac.ir>
2020-02-15 10:41:16 +0330

    Initialize Project

---
+++ source.py
@@ -0,0 +1,9 @@
+"""
+This is my final project in Complex Networks course in Amirkabir university
+In this project we use various techniques for link prediction in Digikala dataset
+Digikala has published some of it's datasets for researchers
+We use Orders table of this dataset that have 200k order in it
+Our result shows that Machnie Learning techniques have best accuracy in this task
+Our source code and result are published for additional research and good purposes
+You can use our code in Colab easily
+"""
